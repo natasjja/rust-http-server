0187a6c1676d3fc0516568e0dcdd30526b772bc5
Natasja Laurie <natasja.laurie@amillionads.com>
2021-11-18 15:06:12 +0000

    infinite loop to keep tcp connection open

--- src/server.rs
+++ src/server.rs
@@ -12,6 +12,10 @@ impl Server {
     pub fn run(self) {
         println!("Listening on {}", self.addr);
 
-        let listener = TcpListener::bind(&self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            listener.accept();
+        }
     }
 }
