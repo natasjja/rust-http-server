8b1e4bf62ef2c3565b60e2bad1d1c482587e1176
Natasja Laurie <natasja.laurie@amillionads.com>
2021-11-19 15:46:35 +0000

    pass buffer to be read

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,5 @@
+use std::io::Read;
 use std::net::TcpListener;
-
 pub struct Server {
     addr: String,
 }
@@ -16,8 +16,15 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Ok((stream, _)) => {
-                    println!("ok")
+                Ok((mut stream, _)) => {
+                    // 1024 bytes enough for this small server
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
                 }
                 Err(e) => println!("Failed to establish connection: {}", e),
             }
