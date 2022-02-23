3ed8dbb9dfe303050fa3d1dd9d22fff7b2e3c882
Natasja Laurie <tasjalaurie@outlook.com>
2022-02-23 16:19:40 +0000

    advanced error handling

--- src/http/request.rs
+++ src/http/request.rs
@@ -2,6 +2,8 @@ use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
@@ -17,7 +19,7 @@ impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        unimplemented!()
+        let request = str::from_utf8(buf)?;
     }
 }
 
@@ -36,6 +38,12 @@ impl ParseError {
             Self::InvalidProtocol => "Invalid Protocol",
             Self::InvalidMethod => "Invalid Method",
         }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
