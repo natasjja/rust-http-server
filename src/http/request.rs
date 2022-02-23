fb23685ff9e3ab401c6700d4cc9b505989a2c48b
Natasja Laurie <tasjalaurie@outlook.com>
2022-02-23 16:54:47 +0000

    iterating over strings

--- src/http/request.rs
+++ src/http/request.rs
@@ -18,9 +18,20 @@ impl Request {
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
+    // GET /search?name=abc&sort=1 HTTP/1.1
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == " " {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+
+    None
 }
 
 pub enum ParseError {
