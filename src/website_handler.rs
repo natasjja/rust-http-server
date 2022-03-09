d4bc011e37b643f0ae43da7861dcebfe423ce959
Natasja Laurie <tasjalaurie@outlook.com>
2022-03-09 14:53:09 +0000

    add routing for incoming requests

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,4 +1,4 @@
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{Method, Request, Response, StatusCode};
 
 use super::server::Handler;
 
@@ -6,6 +6,13 @@ pub struct WebsiteHandler {}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-      Response::new(StatusCode::Ok, Some("<h1>Test</h1>".to_string()))
-  }
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+        }
+    }
 }
