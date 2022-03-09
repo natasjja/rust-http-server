31ece08889564f555c9bc7002c023d6921fda95b
Natasja Laurie <tasjalaurie@outlook.com>
2022-03-09 15:34:20 +0000

    serve html files securely

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use server::Server;
+use std::env;
 use website_handler::WebsiteHandler;
 
 mod http;
@@ -8,8 +9,11 @@ mod server;
 mod website_handler;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+
     let ip: String = "127.0.0.1:8080".to_string();
 
     let server = Server::new(ip);
-    server.run(WebsiteHandler {});
+    server.run(WebsiteHandler { public_path });
 }
